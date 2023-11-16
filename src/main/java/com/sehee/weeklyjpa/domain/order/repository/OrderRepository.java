cfd2f58aec25d7516b84bcf2a563382d87ad5673
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-16 11:05:06 +0900

    feat: OrderRepository method query 정의

--- src/main/java/com/sehee/weeklyjpa/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sehee/weeklyjpa/domain/order/repository/OrderRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
+    List<Order> findAllByOrderStatus(OrderStatus orderStatus);
 
+    List<Order> findAllByOrderStatusOrderByOrderDatetime(OrderStatus orderStatus);
 }
