927fd82ca52b4a2ba147deaf844785d8959c50c3
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-16 20:19:36 +0900

    refactor: 실습에 불필요한 파일 삭제

--- src/main/java/com/sehee/weeklyjpa/domain/customer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sehee.weeklyjpa.domain.customer;
-
-import com.sehee.weeklyjpa.domain.customer.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
