0c9d4aa47bc7dbc98e9c6eace0625dfc94f5207f
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-16 11:16:27 +0900

    feat: OrderRepository named query 정의

--- src/main/java/com/sehee/weeklyjpa/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sehee/weeklyjpa/domain/order/repository/OrderRepository.java
@@ -3,11 +3,16 @@ package com.sehee.weeklyjpa.domain.order.repository;
 import com.sehee.weeklyjpa.domain.order.Order;
 import com.sehee.weeklyjpa.domain.order.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
     List<Order> findAllByOrderStatus(OrderStatus orderStatus);
 
     List<Order> findAllByOrderStatusOrderByOrderDatetime(OrderStatus orderStatus);
+
+    @Query("SELECT o FROM Order AS o WHERE o.memo LIKE %?1%")
+    Optional<Order> findByMemo(String memo);
 }
